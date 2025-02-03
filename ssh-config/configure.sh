3d2ce3250046a2c16a8e4795302d41d448ec24c0
Junior <ymdomenech@gmail.com>
2025-02-03 09:48:15 -0500

    Adding ssh config action

--- ssh-config/configure.sh
+++ ssh-config/configure.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 
 echo "SSH_KEY_NAME=${SSH_KEY_NAME}"
+echo "RELEASE_EMAIL=${RELEASE_EMAIL}"
+echo "REPO=${REPO}"
+
+set -x
+
+mkdir -p ~/.ssh
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -C "${RELEASE_EMAIL}"
+
+SSH_PRIV=$(cat ~/.ssh/id_ed25519);
+#SSH_PUB=$(cat ~/.ssh/id_ed25519.pub);
+
+
+
+gh secret set "${SSH_KEY_NAME}" --body "${SSH_PRIV}" --repo "${REPO}"
+
+#gh repo deploy-key add ~/.ssh/id_ed25519.pub --title "release" --read-only --repo "${REPO}"
+
+
+
+
